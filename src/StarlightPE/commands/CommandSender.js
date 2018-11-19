89f6a555e68bd475d6fa2fc38fcd1bf39296caee
EmeraldMC <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-11-19 15:46:08 -0600

    Create CommandSender.js

---
+++ src/StarlightPE/commands/CommandSender.js
@@ -0,0 +1,15 @@
+class CommandSender {
+    constructor(server){
+        this.server = server;
+    }
+
+    hasPermission(){
+
+    }
+
+    getServer(){
+        return this.server;
+    }
+}
+
+module.exports = CommandSender;
