1550a5f8f6b4abb4a77eafbaf3e69da25660d53c
EmeraldMC <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-11-19 15:48:06 -0600

    Create ConsoleCommandReader.js

---
+++ src/StarlightPE/commands/ConsoleCommandReader.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const ConsoleCommandSender = starlightpe("command/ConsoleCommandSender");
+
+class ConsoleCommandReader {
+    constructor(Server){
+        let CommandSender = new ConsoleCommandSender(Server);
+        let rl = readline.createInterface({
+            input: process.stdin
+        });
+
+        rl.on("line", (input) => {
+            Server.getCommandMap().dispatchCommand(CommandSender, input);
+        });
+    }
+}
+
+module.exports = ConsoleCommandReader;
