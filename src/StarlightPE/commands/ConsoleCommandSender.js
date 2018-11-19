c7ab97c86d240e3b98c5a08cc776c9b64c1dbe76
EmeraldMC <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-11-19 15:49:16 -0600

    Create ConsoleCommandSender.js

---
+++ src/StarlightPE/commands/ConsoleCommandSender.js
@@ -0,0 +1,21 @@
+const CommandSender = starlightpe("command/CommandSender");
+
+class ConsoleCommandSender extends CommandSender {
+    constructor(server){
+        super(server);
+    }
+
+    sendMessage(message){
+        this.server.getLogger().info(message);
+    }
+
+    getName(){
+        return "CONSOLE";
+    }
+
+    isOp(){
+        return true;
+    }
+}
+
+module.exports = ConsoleCommandSender;
